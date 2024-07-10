fe56367295bf1baf8d4439c9aba9b21a4697ba8c
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2024-07-10 20:00:03 +0300

    HomePage

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -2,15 +2,15 @@ import NavBarAuth from "./NavBarAuth/NavBarAuth.jsx";
 import MainMenu from "./NavBarMenu/NavBarMenu.jsx";
 import css from "./navbar.module.css";
 import { Link } from "react-router-dom";
-import NavBarUser from "./NavBarUser/NavBarUser.jsx"
-import {RxHamburgerMenu} from "react-icons/rx"; 
+import NavBarUser from "./NavBarUser/NavBarUser.jsx";
+import { RxHamburgerMenu } from "react-icons/rx";
 import { IoMdClose } from "react-icons/io";
 import { useState } from "react";
 const Navbar = () => {
-  const [ nav , setNav]= useState(false)
-  const isLogin = false
+  const [nav, setNav] = useState(false);
+  const isLogin = false;
   return (
-    <div className={css.conteiner}>      
+    <div className={css.conteiner}>
       <nav className={css.nav_list}>
         <Link className={css.flag_link}>
           <div className={css.flag_circle}>
@@ -20,16 +20,17 @@ const Navbar = () => {
           <span className={css.text}>LearnLingo</span>
         </Link>
       </nav>
-      
+
       <MainMenu />
-      <div onClick={()=>setNav(!nav)}className={css.mb_btn}> 
-        {nav ?  <IoMdClose size={36} /> :  < RxHamburgerMenu  size={36}/>  }
-      
-     
-       </div>
-       <div  className={nav? [css.burger, css.active].join(" "):[css.burger]} >{isLogin ? <NavBarUser /> : <NavBarAuth /> } </div>
-      
-    
+      <div
+        onClick={() => setNav(!nav)}
+        className={css.mb_btn}
+      >
+        {nav ? <IoMdClose size={36} /> : <RxHamburgerMenu size={36} />}
+      </div>
+      <div className={nav ? [css.burger, css.active].join(" ") : [css.burger]}>
+        {isLogin ? <NavBarUser /> : <NavBarAuth />}{" "}
+      </div>
     </div>
   );
 };
---
+++ src/components/Page/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import css from "./home-page.module.css"
+
+const HomePage = ()=> {
+    return (<>
+    </>
+        
+    )
+}
+
+
+export default HomePage ; 
