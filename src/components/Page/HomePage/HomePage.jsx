6f861f804761df0b3db781ca860875c64f59196c
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2024-07-11 20:04:04 +0300

    hero

--- src/components/Page/HomePage/HomePage.jsx
+++ src/components/Page/HomePage/HomePage.jsx
@@ -2,6 +2,10 @@ import css from "./home-page.module.css"
 
 const HomePage = ()=> {
     return (<>
+    <div className={css.container} >
+          <div className={css.info_container}  > </div>
+          <div></div>
+    </div>
     </>
         
     )
